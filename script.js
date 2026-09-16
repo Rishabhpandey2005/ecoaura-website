5d355dcfef3d7f4e5c3bb6174417e7d3cab957bd
RISHABH PANDEY <winterarcnewman@gmail.com>
2026-09-16 18:14:41 +0000

    Intial ecoaura website

---
+++ script.js
@@ -0,0 +1,12 @@
+function showProduct(productName) {
+    alert(
+        productName +
+        " will be available soon. EcoAura is currently building its product range."
+    );
+}
+
+function contactMessage() {
+    alert(
+        "Thank you for your interest in EcoAura! Our contact system is coming soon."
+    );
+}
